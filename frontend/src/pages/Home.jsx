a1532d0223ed7bfd2b24029431eb750d8053b04a
Ayawzer <kacper.bryla06@gmail.com>
2024-08-03 20:05:36 +0200

    its a start

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import logo from '../assets/logo.png';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-[#1A3341] text-white">
+      
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center p-2">
+    <a href="/" class="flex items-center">
+        <img src={logo} class="h-14" alt="Logo" />
+    </a>
+  </div>
+</nav>
+
+    </div>
+  )
+}
